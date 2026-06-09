5068809f5e92e521df22a81ec5359e918be60322
sboyarkin <sergey.boyarkin@mail.ru>
2026-06-09 16:58:23 +0500

    allowed host esvapi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1hono19vbsq6-n8y%&+(q58@1im=-&+%f=(y(v7f)5)v-tg7xm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['esvapi']
+ALLOWED_HOSTS = ['esvapi', '172.20.7.73']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 
